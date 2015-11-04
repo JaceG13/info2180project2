72b79dd62114167d656a570e6e535c509d29fe4c
jaceg13 <jaygooden@hotmail.com>
2015-11-04 23:57:04 +0000

    added picture and layout

--- fifteen.js
+++ fifteen.js
@@ -1,11 +1,8 @@
-var space = "300";
-var top = 0;
-var left = 0;
-var pieces = $$("div#puzzlearea div");
-
 window.onload = function(){
     setPieces();
 };
+
+//var space = "300px 300px";
 
 /*  pieces[0].style.backgroundPosition = "0px 0px";
     pieces[1].style.backgroundPosition = "-100px 0px";
@@ -30,27 +27,46 @@ window.onload = function(){
         }
     }
 };*/
+var pieces = $$("div#puzzlearea div");
 
 var setPieces = function(){
+    var l = 0;
+    var t = 0;
+    var pieces = $$("div#puzzlearea div");
     for (var i = 0; i < pieces.length; i++){
         pieces[i].addClassName("puzzlepiece");
+        pieces[i].style.left = "" + (l*100) + "px";
+        pieces[i].style.top = "" + (t*100) + "px";
+        pieces[i].style.backgroundPosition = "-" + (l*100) + "px -" + (t*100) + "px";
+        //pieces[i].addClassName("movablepiece");
         
-        /*do{
-            do{
-                pieces[i].style.position = "" + left + "px " + top + " px";
-                left += 100;
-            }while(left <= 300);
-        }while(top <=300);*/
-        
-        if(left <= 300){
-            pieces[i].style.position = "" + left + "px " + top + " px";
-        }else{
-            left = 0;
-            top += 100;
-            pieces[i].style.position = "" + left + "px " + top + " px";
+        if (l == 3){
+            l = -1;
+            t++;
         }
         
-        left += 100;
+        l++;
+        //check(pieces[i]);
+        
+        //console.log(pieces[i].style.left);
+        //console.log(pieces[i].style.top);
+        //console.log(pieces[i].style.backgroundPosition);
     }
+    //var space = pieces[0];
+    //space.removeClassName("puzzlepiece");
+    //space.style.width = "100px";
+    //space.style.height = "100px";
+    //space.style.position = "300px 300px";
+    //pieces.push(space);
     //shuffle();
 };
+
+var top = "300px";
+var left = "300px";
+
+var check = function(piece){
+    if (piece.style.left == left || piece.style.top == top){
+        piece.addClassName("movablepiece");
+    }
+}
+
