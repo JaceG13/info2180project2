5c306a349671eff4c656a361456279e5978be7fd
jaceg13 <jaygooden@hotmail.com>
2015-11-05 23:29:51 +0000

    added functionality for checking movable pieces and attempted movePiece

--- fifteen.js
+++ fifteen.js
@@ -1,11 +1,24 @@
 window.onload = function(){
     setPieces();
-    movable();
-    $$("div#puzzlearea div.movablepiece").onclick = movePiece(0);
+    
+    //var movablePieces = $$("div#puzzlearea div.movablepiece");
+    //movablePieces.addEventListener("click", movePiece());
+    
+    for(var j = 0; j < allPieces.length; j++){
+        var pos = j;
+        if(allPieces[pos].classList.contains("movablepiece")){
+            movablePieces.push(allPieces[j]);
+        }
+    }
+    for(var k = 0; k < movablePieces.length; k++){
+        console.log(movablePieces[k]);
+        movablePieces[k].addEventListener("click", movePiece(movablePieces[k]));
+    }
 };
 
 var allPieces;
-var movablePieces;
+var movablePieces;// = $$("div#puzzlearea div.movablepiece");
+
 var space = "300px 300px";
 
 var setPieces = function(){
@@ -31,20 +44,44 @@ var setPieces = function(){
         //console.log(pieces[i].style.backgroundPosition);
     }
     allPieces = pieces;
+    movable();
 };
+
+var spaceTop = "300px";
+var spaceLeft = "300px";
 
 var movable = function(){
     var pieces = allPieces;
     for (var j = 0; j < pieces.length; j++){
-        pieces[j].addClassName("movablepiece");
+        if(pieces[j].style.top === spaceTop){
+           if(Math.abs(parseInt(pieces[j].style.left, 10) - parseInt(spaceLeft, 10)) === 100){
+               pieces[j].addClassName("movablepiece");
+           }
+        }
+        
+        if(pieces[j].style.left === spaceLeft){
+            if(Math.abs(parseInt(pieces[j].style.top, 10) - parseInt(spaceTop, 10)) === 100){
+                pieces[j].addClassName("movablepiece");
+            }
+        }
+        //pieces[j].addClassName("movablepiece");
     }
-    movablePieces = $$("div#puzzlearea div.movablepiece");
+    //movablePieces = $$("div#puzzlearea div.movablepiece");
+    allPieces = pieces;
 };
 
-var movePiece = function(pos){
-    var pieces = allPieces;
-    var temp = allPieces[pos].style.position;
-    allPieces[pos].style.position = space;
-    space = temp;
+var movePiece = function(piece){
+    
+    var tempTop = piece.style.top;
+    var tempLeft = piece.style.left;
+    
+    //if(piece.classList.contains("movablepiece")){
+    piece.style.top = spaceTop;
+    piece.style.left = spaceLeft;
+    spaceTop = tempTop;
+    spaceLeft = tempLeft;
+    //}
+    
+    movable();
 };
 
